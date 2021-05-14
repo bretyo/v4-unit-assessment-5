c6e7f5b28f5775352e30d17e36e48ee9e45471bf
Bret Fernberg <bret.fernberg@gmail.com>
2021-05-14 09:49:23 -0600

    Saving progress

--- server/index.js
+++ server/index.js
@@ -1,12 +1,32 @@
 require('dotenv').config();
+const massive = require('massive');
 const express = require('express'),
       userCtrl = require('./controllers/user'),
       postCtrl = require('./controllers/posts')
 
+// DESTRUCTURING
+const {SERVER_PORT, CONNECTION_STRING} = process.env;
 
 const app = express();
 
+// TOP LEVEL MIDDLEWARE
 app.use(express.json());
+
+
+// MASSIVE
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {
+        rejectUnauthorized: false
+    }
+})
+.then(()=>{
+    app.set('db', db)
+    console.log('Database Connected!')
+    app.listen(SERVER_PORT, _ => console.log(`running on ${SERVER_PORT}`));
+})
+
+
 
 //Auth Endpoints
 app.post('/api/auth/register', userCtrl.register);
@@ -19,5 +39,3 @@ app.get('/api/posts', postCtrl.readPosts);
 app.post('/api/post', postCtrl.createPost);
 app.get('/api/post/:id', postCtrl.readPost);
 app.delete('/api/post/:id', postCtrl.deletePost)
-
-app.listen(4000, _ => console.log(`running on ${4000}`));
